ac4aa93bcdb56049a30b517c56e9e82ffcf9fcd7
Aniket Singh <asaniket.singh422@gmail.com>
2026-06-26 16:04:08 +0530

    Ques 7 day4

---
+++ Logical Operators & Compound Statement/ques7.java
@@ -0,0 +1,64 @@
+
+import java.util.Scanner;
+
+// Ques:Take a single digit (0-9)and print it word from 0-9
+public class ques7 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int digit=sc.nextInt();
+
+        switch (digit) {
+            case 0:
+                System.out.println("zero");
+                break;
+
+             case 1:
+                System.out.println("one");
+                break;   
+
+            case 2:
+                System.out.println("two");
+                break;    
+            
+            case 3:
+                System.out.println("three");
+                break;
+                
+            case 0:
+                System.out.println("zero");
+                break;
+                
+                
+            case 4:
+                System.out.println("four");
+                break;  
+                
+                
+             case 5:
+                System.out.println("five");
+                break; 
+                
+            case 6:
+                System.out.println("six");
+                break;    
+
+            case 7:
+                System.out.println("seven");
+                break;
+             
+            case 8:
+                System.out.println("eight");
+                break; 
+                
+            case 9:
+                System.out.println("nine");
+                break;
+                
+            default:
+                System.out.println("Invalid Digit");
+              
+        }
+
+
+    }
+}
