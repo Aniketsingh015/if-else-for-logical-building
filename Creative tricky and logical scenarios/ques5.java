e15182bed42070c204b2b70474edd7edc3544d41
Aniket Singh <asaniket.singh422@gmail.com>
2026-07-01 20:58:31 +0530

    ques3 day 5

--- Creative tricky and logical scenarios/ques5.java
+++ Creative tricky and logical scenarios/ques5.java
@@ -1,5 +1,26 @@
-// Check whether three numbers are in Arithmetic Progression (AP).
-
+// Check whether three numbers are in Arithmetic Progression (AP).diffrence between consecutive numbers is same.
+import java.util.Scanner;
 public class ques5 {
     
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter first number: ");
+        int a = sc.nextInt();
+
+        System.out.print("Enter second number: ");
+        int b = sc.nextInt();
+
+        System.out.print("Enter third number: ");
+        int c = sc.nextInt();
+
+        if ((b - a) == (c - b)) {
+            System.out.println("Arithmetic Progression");
+        } else {
+            System.out.println("Not an Arithmetic Progression");
+        }
+    }
+
 }
