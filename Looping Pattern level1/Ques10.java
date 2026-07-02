6862f3c6e46c62e2fc98ffc79329ef9d0e0c32d2
Aniket Singh <asaniket.singh422@gmail.com>
2026-07-02 11:23:23 +0530

    Looping pattern level 1 ques 10

--- Looping Pattern level1/Ques10.java
+++ Looping Pattern level1/Ques10.java
@@ -1,5 +1,26 @@
-// Ques: Print all even numbers between 1 and 100
+// Ques: Print the product of digits of a given number
 import java.util.*;
 public class Ques10 {
-    
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number: ");
+
+        int num = sc.nextInt();
+
+        int product = 1;
+
+        while (num > 0) {
+
+            int digit = num % 10;
+
+            product = product * digit;
+
+            num = num / 10;
+
+        }
+
+        System.out.println("Product = " + product);
+}
 }
