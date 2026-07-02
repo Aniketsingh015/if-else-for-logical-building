cf6588a5bc5eb893f48a00d431439a33ad65f9dd
Aniket Singh <asaniket.singh422@gmail.com>
2026-07-02 11:20:37 +0530

    Looping pattern level 1 ques 7

--- Looping Pattern level1/Ques7.java
+++ Looping Pattern level1/Ques7.java
@@ -1,5 +1,27 @@
-// Ques: Print all even numbers between 1 and 100
+// Ques: Print the sum of all even numbers up to n
 import java.util.*;
 public class Ques7 {
-    
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter n: ");
+
+        int n = sc.nextInt();
+
+        int i = 2;
+
+        int sum = 0;
+
+        while (i <= n) {
+
+            sum = sum + i;
+
+            i += 2;
+
+        }
+
+        System.out.println("Sum = " + sum);
+
+    }
 }
