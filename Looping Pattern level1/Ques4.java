e7f7fba401ce7d7019b70c43c3e6fae2d35a8a3e
Aniket Singh <asaniket.singh422@gmail.com>
2026-07-02 11:17:32 +0530

    Looping pattern level 1 ques 4

--- Looping Pattern level1/Ques4.java
+++ Looping Pattern level1/Ques4.java
@@ -1,6 +1,18 @@
-// Ques: Print all even numbers between 1 and 100
-import java.util.*;
+// Ques: PPrint numbers from 10 down to 1
 
 public class Ques4 {
+    public static void main(String[] args) {
+
+        int i = 10;
+
+        while (i >= 1) {
+
+            System.out.println(i);
+
+            i--;
+
+        }
+
+    }
     
 }
