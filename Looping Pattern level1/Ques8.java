57f39044b2bf63d450ba4ad75736b0a4b8071789
Aniket Singh <asaniket.singh422@gmail.com>
2026-07-02 11:21:50 +0530

    Looping pattern level 1 ques 8

--- Looping Pattern level1/Ques8.java
+++ Looping Pattern level1/Ques8.java
@@ -1,6 +1,28 @@
-// Ques: Print all even numbers between 1 and 100
+// Ques: Print the sum of all odd numbers up to n
 import java.util.*;
 
 public class Ques8 {
-    
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter n: ");
+
+        int n = sc.nextInt();
+
+        int i = 1;
+
+        int sum = 0;
+
+        while (i <= n) {
+
+            sum = sum + i;
+
+            i += 2;
+
+        }
+
+        System.out.println("Sum = " + sum);
+
+    }
 }
