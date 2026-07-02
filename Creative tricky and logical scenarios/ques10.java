45c8fc5a776a2f061f0a853c95b307ceaa802b01
Aniket Singh <asaniket.singh422@gmail.com>
2026-07-02 13:04:16 +0530

    ques 10 day 5

--- Creative tricky and logical scenarios/ques10.java
+++ Creative tricky and logical scenarios/ques10.java
@@ -1,5 +1,22 @@
 // Print the corresponding century of a year.
-
+// year 2000 belongs to 20th century
+// year 2001 belongs to 21st century
+// year 2024 belongs to 21st century
+// year 1900 belongs to 19th century-here if divided by 100 it will give 19 but it belongs to 19th century 
+// year 1901 belongs to 20th century-if not divisible by100 then we will add 1 to the quotient of year/100
+import java.util.*;
 public class ques10 {
-    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter year: ");
+        int year = sc.nextInt();
+
+        if(year % 100 ==0){
+            System.out.println("Century = "+year/100);
+        }
+        else{
+            System.out.println("Century = "+(year/100+1));
+
+        }
+    }
 }
