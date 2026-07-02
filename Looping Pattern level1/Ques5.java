c3bdfae3a48cbd376b9b9cc785b1064fcf6e79a4
Aniket Singh <asaniket.singh422@gmail.com>
2026-07-02 11:18:44 +0530

    Looping pattern level 1 ques 5

--- Looping Pattern level1/Ques5.java
+++ Looping Pattern level1/Ques5.java
@@ -1,5 +1,23 @@
-// Ques: Print all even numbers between 1 and 100
+// Ques: Print the table of a given number
 import java.util.*;
 public class Ques5 {
-    
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number: ");
+
+        int num = sc.nextInt();
+
+        int i = 1;
+
+        while (i <= 10) {
+
+            System.out.println(num + " x " + i + " = " + (num * i));
+
+            i++;
+
+        }
+
+    }
 }
