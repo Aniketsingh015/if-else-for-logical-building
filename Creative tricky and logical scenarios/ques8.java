935b848559d13dd506b24f43a5e0b016ad7ef3f5
Aniket Singh <asaniket.singh422@gmail.com>
2026-07-02 12:58:51 +0530

    ques 8 day 5

--- Creative tricky and logical scenarios/ques8.java
+++ Creative tricky and logical scenarios/ques8.java
@@ -1,5 +1,37 @@
 // Check whether sum of digits is greater than product of digits.
 
+import java.util.Scanner;
+
 public class ques8 {
-    
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int num = sc.nextInt();
+
+        int sum = 0;
+
+        int product = 1;
+
+        while (num > 0) {
+
+            int digit = num % 10;
+
+            sum += digit;
+
+            product *= digit;
+
+            num /= 10;
+
+        }
+
+        if (sum > product)
+
+            System.out.println("Sum is greater");
+
+        else
+
+            System.out.println("Product is greater or equal");
+
+    }
 }
