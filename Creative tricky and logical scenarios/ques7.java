812e5fa0870d1c21fd2a4085898cd65b379d1b11
Aniket Singh <asaniket.singh422@gmail.com>
2026-07-02 12:58:11 +0530

    ques 7 day 5

--- Creative tricky and logical scenarios/ques7.java
+++ Creative tricky and logical scenarios/ques7.java
@@ -1,5 +1,22 @@
+
+import java.util.Scanner;
+
 // Check if first digit + last digit = middle digit (3-digit number).
 
 public class ques7 {
-    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a 3-digit number: ");
+        int num = sc.nextInt();
+
+        int firstDigit = num / 100; // Extract the first digit
+        int lastDigit = num % 10; // Extract the last digit
+        int middleDigit = (num / 10) % 10; // Extract the middle digit      
+
+        if (firstDigit + lastDigit == middleDigit) {
+            System.out.println("The sum of the first and last digits is equal to the middle digit.");
+        } else {
+            System.out.println("The sum of the first and last digits is NOT equal to the middle digit.");
+        }
+    }
 }
