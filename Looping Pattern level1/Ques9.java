8bc68f773f0c26bb68f6afa695fb4dd181061b72
Aniket Singh <asaniket.singh422@gmail.com>
2026-07-02 11:22:29 +0530

    Looping pattern level 1 ques 9

--- Looping Pattern level1/Ques9.java
+++ Looping Pattern level1/Ques9.java
@@ -1,5 +1,27 @@
-// Ques: Print all even numbers between 1 and 100
+// Ques: Print the factorial of a given number
 import java.util.*;
 public class Ques9 {
-    
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number: ");
+
+        int n = sc.nextInt();
+
+        int i = 1;
+
+        long fact = 1;
+
+        while (i <= n) {
+
+            fact = fact * i;
+
+            i++;
+
+        }
+
+        System.out.println("Factorial = " + fact);
+
+    }
 }
