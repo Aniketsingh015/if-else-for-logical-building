3ab916303a93231a41b64570894d918c608c910b
Aniket Singh <asaniket.singh422@gmail.com>
2026-07-02 11:07:58 +0530

    Looping pattern level 1 ques 2

--- Looping Pattern level1/Ques2.java
+++ Looping Pattern level1/Ques2.java
@@ -1,6 +1,31 @@
 // Ques: Print all even numbers between 1 and 100
-import java.util.*;
 
 public class Ques2 {
-    
+    // approach one:we can go for checking the numbers and then 
+    // printing the even numbers but here computer have to check each and every number
+    //  but you know the logic of even number so directy print
+    public static void main(String[] args) {
+        
+    // int i = 1;
+
+    //     while (i <= 100) {
+
+    //         if (i % 2 == 0) {
+
+    //             System.out.println(i);
+
+    //         }
+
+    //         i++;
+
+    //     }
+
+    //approach 2 directly check the even number
+    int i=2;
+
+    while(i<=100){
+        System.out.println(i);;
+        i+=2;
+    }
+}
 }
