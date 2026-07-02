98680b293118eecb592352961c221852595aa0cf
Aniket Singh <asaniket.singh422@gmail.com>
2026-07-02 11:10:02 +0530

    Looping pattern level 1 ques 3

--- Looping Pattern level1/Ques3.java
+++ Looping Pattern level1/Ques3.java
@@ -1,6 +1,18 @@
-// Ques: Print all even numbers between 1 and 100
-import java.util.*;
+// Ques: Print all odd numbers between 1 and 100
 
 public class Ques3 {
-    
+    public static void main(String[] args) {
+        //approach 1: we can go for checking the numbers and then printing the odd numbers
+        for(int i=1;i<=100;i++){
+            if(i%2!=0){
+                System.out.println(i);  
+
+    }
+}
+
+// approach 2: directly check the odd number
+for(int i=1;i<=100;i+=2){
+    System.out.println(i);      
+    }
+}
 }
