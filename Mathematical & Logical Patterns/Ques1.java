8325b899d6bf50ca4c7d818e60df4f45fe7bcb79
Aniket Singh <asaniket.singh422@gmail.com>
2026-07-05 21:06:00 +0530

    Ques1 day7

---
+++ Mathematical & Logical Patterns/Ques1.java
@@ -0,0 +1,18 @@
+// print sq of number from 1 to n code
+// we can use Math.pow()metyhod but remeber it retrn double value so we have to cast it into int
+
+import java.util.*;
+public class Ques1 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter n: ");
+        int n = sc.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+            System.out.println(i * i);
+        }
+    }
+}
+
