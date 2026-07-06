54d89b69442ab7010e2c24fb72fc31b3ddeba5bd
Aniket Singh <asaniket.singh422@gmail.com>
2026-07-06 09:43:38 +0530

    Ques 3 day8

---
+++ Pattern printing/Ques3.java
@@ -0,0 +1,15 @@
+// Print Square of Stars (n x n Stars)
+import java.util.*;
+
+public class Ques3 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+    }
+}
