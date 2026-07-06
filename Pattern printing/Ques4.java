fbf73b0423128ab998d8ab3aa9c11fe242f2d13a
Aniket Singh <asaniket.singh422@gmail.com>
2026-07-06 09:46:08 +0530

    Ques 4 day8

---
+++ Pattern printing/Ques4.java
@@ -0,0 +1,22 @@
+// Print an Increasing Triangle of Stars
+/* 
+*
+**
+***
+****
+*****
+
+*/
+
+public class Ques4 {
+    public static void main(String[] args) {
+        
+    
+    for(int i=0;i<5;i++){
+        for(int j=0;j<=i;j++){
+            System.out.print("*");
+        }
+        System.out.println("");
+    }
+}
+}
