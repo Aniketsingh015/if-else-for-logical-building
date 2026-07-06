864cb78bec1de901ade9de2817c31f5257afef78
Aniket Singh <asaniket.singh422@gmail.com>
2026-07-06 09:54:39 +0530

    Ques6 day 8

---
+++ Pattern printing/Ques6.java
@@ -0,0 +1,18 @@
+// print stars in even number
+/* 
+**
+****
+******
+********
+**********
+*/
+public class Ques6 {
+public static void main(String[] args) {
+    for(int i=1;i<=5;i++){
+        for(int j=1;j<=i*2;j++){
+            System.out.print("*");
+        }
+        System.out.println("");
+    }
+}    
+}
