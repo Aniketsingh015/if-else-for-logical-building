660347dbeb2953d71413eea7710d7edbc48e0412
Aniket Singh <asaniket.singh422@gmail.com>
2026-07-20 13:52:34 +0530

    Ques1

---
+++ RecursionLevel1/Ques1.java
@@ -0,0 +1,19 @@
+// print number from 1 to n
+
+import java.util.Scanner;
+
+public class Ques1 {
+
+    public static void print1toN(int n){
+        if(n==0){
+            return;
+        }
+        print1toN(n-1);
+        System.out.println(n);
+    }
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        print1toN(n);
+    }
+}
