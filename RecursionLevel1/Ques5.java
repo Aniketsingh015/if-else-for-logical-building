172c0d9c50775b1a2b691d5c2114884b011c8d9e
Aniket Singh <asaniket.singh422@gmail.com>
2026-08-04 14:14:19 +0530

    Ques5 fresh

---
+++ RecursionLevel1/Ques5.java
@@ -0,0 +1,12 @@
+public class Ques5 {
+    public static int sumHelper(int n){
+        // if(n==0)return sum;
+        // sum+=n;
+        // return sumHelper(n-1, sum);
+        if(n==0)return 0;
+        return n+sumHelper(n-1);
+    }
+    public static void main(String[] args) {
+        System.out.println(sumHelper(10));
+    }
+}
