dcf4798f1edb116e26eaa818ccf804640effcd42
Aniket Singh <asaniket.singh422@gmail.com>
2026-08-04 12:48:32 +0530

    Ques1 fresh

--- RecursionLevel1/Ques1.java
+++ RecursionLevel1/Ques1.java
@@ -1,20 +1,10 @@
-
-// print number from 1 to n
-
-import java.util.Scanner;
-
 public class Ques1 {
-
-    public static void print1toN(int n){
-        if(n==0){
-            return;
-        }
-        print1toN(n-1);
-        System.out.println(n);
+    public static void printHelper(int i,int n){
+        if(i>n)return;
+        System.out.print(i);
+        printHelper(i+1, n-1);
     }
     public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-        int n=sc.nextInt();
-        print1toN(n);
+        printHelper(1,10);
     }
 }
