35bde75f84fdfe9aa6d29e43c9a4593b3c9035c4
Aniket Singh <asaniket.singh422@gmail.com>
2026-08-10 17:55:39 +0530

    Ques 3

---
+++ arraysLevel1/Ques3.java
@@ -0,0 +1,12 @@
+// Find the average of array elements.
+public class Ques3 {
+    public static void main(String[] args) {
+        int arr[]={2,3,5,8,9};
+        int sum=0;
+        for (int i = 0; i < arr.length; i++) {
+            sum+=arr[i];
+        }
+        double avg=(double)sum/arr.length;
+        System.out.println(avg);
+    }
+}
