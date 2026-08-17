4ff6af339877b9ae962a131d276f5aa07d4b00b7
Aniket Singh <asaniket.singh422@gmail.com>
2026-08-17 10:56:27 +0530

    Ques

--- arraysLevel2/Ques3.java
+++ arraysLevel2/Ques3.java
@@ -3,7 +3,7 @@ public class Ques3 {
     public static void main(String[] args) {
         int arr[]={2,4,6,8,9,7,6};
         int x=6;
-        int indx=0;
+        int indx=-1;
 
         for(int i=0;i<arr.length;i++){
             if(arr[i]==x){
@@ -11,6 +11,10 @@ public class Ques3 {
                 break;
             }
         }
-        System.out.println(indx);
+        if(indx==-1){
+            System.out.println("value not found");
+        }else{
+            System.out.println(indx);
+        }
     }
 }
