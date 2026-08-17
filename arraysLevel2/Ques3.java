b09bab1a98ace903ace2027be320b2e106ce8378
Aniket Singh <asaniket.singh422@gmail.com>
2026-08-17 10:44:25 +0530

    Ques

--- arraysLevel2/Ques3.java
+++ arraysLevel2/Ques3.java
@@ -1,1 +1,16 @@
 // Find the first occurrence of a given number.
+public class Ques3 {
+    public static void main(String[] args) {
+        int arr[]={2,4,6,8,9,7,6};
+        int x=6;
+        int indx=0;
+
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]==x){
+                indx=i;
+                break;
+            }
+        }
+        System.out.println(indx);
+    }
+}
