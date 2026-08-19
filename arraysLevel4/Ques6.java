52361de38764be22f2ef147779e9166506a8857e
Aniket Singh <asaniket.singh422@gmail.com>
2026-08-19 14:46:39 +0530

    Ques

---
+++ arraysLevel4/Ques6.java
@@ -0,0 +1,16 @@
+
+public class Ques6 {
+    public static void main(String[] args) {
+        int arr1[]={2,3,4,5,6};
+        int arr2[]={1,2,8,9,10};
+        int count=0;
+
+        for(int i=0;i<arr1.length;i++){
+            for(int j=0;j<arr2.length;j++){
+                if(arr1[i]==arr2[j]){
+                    count++;
+                }
+            }
+        }System.out.println(count);
+    }
+}
