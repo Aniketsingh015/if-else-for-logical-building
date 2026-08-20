27879d93a34a0127d0ca84b470b645fef3c32907
Aniket Singh <asaniket.singh422@gmail.com>
2026-08-20 10:28:27 +0530

    Ques

---
+++ arraysLevel5/Ques8.java
@@ -0,0 +1,20 @@
+
+public class Ques8 {
+    public static void main(String[] args) {
+        int arr[]={2,4,5,6,7};
+        int sum=0;
+        int avg=0;
+        int count=0;
+        for(int i=0;i<arr.length;i++){
+            sum+=arr[i];
+        }
+        avg=sum/arr.length;
+
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]>avg){
+                count++;
+            }
+        }
+        System.out.println(count);
+    }
+}
