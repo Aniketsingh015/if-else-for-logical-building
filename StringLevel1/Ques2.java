17b887c2e8380847a2875cf7fbef8803fb472dea
Aniket Singh <asaniket.singh422@gmail.com>
2026-08-21 14:44:45 +0530

    will be modified

--- StringLevel1/Ques2.java
+++ StringLevel1/Ques2.java
@@ -1,12 +1,3 @@
-import java.util.*;
-
-public class Ques2 {
-    public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-        String s=sc.nextLine();
-
-        System.out.println(s.charAt(0));
-        System.out.println(s.charAt(s.length()-1));
-
-    }
+class Ques2{
+    
 }
