8fdb157abefcc2b3d72030f7f25ac01a3e230f75
Aniket Singh <asaniket.singh422@gmail.com>
2026-08-21 17:12:57 +0530

    push

---
+++ StringLevel1/Ques7.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Ques7 {
+   public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        String s=sc.nextLine();
+        String v=sc.nextLine();
+
+        System.out.println(s.concat(v));
+        System.out.println(s +" "+v);
+
+   } 
+}
