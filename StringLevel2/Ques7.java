054a420eec2d9f7955770adfe1dfc5a102c2cdba
Aniket Singh <asaniket.singh422@gmail.com>
2026-08-22 12:58:37 +0530

    ques7

--- StringLevel2/Ques7.java
+++ StringLevel2/Ques7.java
@@ -1,5 +1,19 @@
-package StringLevel2;
 
+// a-z->97-122
+// A->Z ->65-90
 public class Ques7 {
-    
+    public static void main(String[] args) {
+        String s="Apple";
+        int beforeCount=0;
+        int afterCount=0;
+        for(int i=0;i<s.length();i++){
+            if(s.charAt(i)< 'm'){
+                beforeCount++;
+            }else{
+                afterCount++;
+            }
+        }
+        System.out.println(beforeCount);
+        System.out.println(afterCount);
+    }
 }
