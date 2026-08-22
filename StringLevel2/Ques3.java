ce9ad41cbbab423937f87ae8c2c96c682012c12b
Aniket Singh <asaniket.singh422@gmail.com>
2026-08-22 12:29:03 +0530

    ques3

--- StringLevel2/Ques3.java
+++ StringLevel2/Ques3.java
@@ -1,5 +1,19 @@
-package StringLevel2;
 
 public class Ques3 {
-    
+    public static void main(String[] args) {
+        String s="Hello World";
+        int lowercase=0;
+        int uppercase=0;
+
+        for(int i=0;i<s.length();i++){
+            char c=s.charAt(i);
+            if(Character.isLowerCase(c)){
+                lowercase++;
+            }else if(Character.isUpperCase(c)){
+                uppercase++;
+            }
+        }
+        System.out.println(lowercase);
+        System.out.println(uppercase);
+    }
 }
