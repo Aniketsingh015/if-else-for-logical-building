9fe0cd2965db55133a2cdfb2d1d91d5c6c75c3c5
Aniket Singh <asaniket.singh422@gmail.com>
2026-08-22 13:11:46 +0530

    puhs

--- StringLevel2/Ques9.java
+++ StringLevel2/Ques9.java
@@ -1,5 +1,17 @@
-package StringLevel2;
-
 public class Ques9 {
-    
+    public static void main(String[] args) {
+        String s = "apple and orange";
+        int count = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            char v = Character.toLowerCase(s.charAt(i));
+
+            if ((i == 0 || s.charAt(i - 1) == ' ') &&
+                (v == 'a' || v == 'e' || v == 'i' || v == 'o' || v == 'u')) {
+                count++;
+            }
+        }
+
+        System.out.println(count);
+    }
 }
