b7f4eb7a5b4940d888bb0e085650bc8b36fd8566
Aniket Singh <asaniket.singh422@gmail.com>
2026-08-22 12:36:53 +0530

    Ques4

--- StringLevel2/Ques4.java
+++ StringLevel2/Ques4.java
@@ -1,5 +1,29 @@
-package StringLevel2;
+
 
 public class Ques4 {
-    
+    public static void main(String[] args) {
+        
+        String str="Hello world";
+        for(int i=0;i<str.length();i++){
+            boolean seenbefore=false;
+            for(int j=0;j<i;j++){
+                if(str.charAt(i)==str.charAt(j)){
+                seenbefore=true;
+                break;
+            }
+            }
+
+            if(!seenbefore){
+                int count=0;
+                for(int k=0;k<str.length();k++){
+                    if(str.charAt(i)==str.charAt(k)){
+                        count++;
+                    }
+                }
+
+                System.out.println(str.charAt(i)+ "count is" + '=' + count) ;
+            }
+
+        }
+    }
 }
