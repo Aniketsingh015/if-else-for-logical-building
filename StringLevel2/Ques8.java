fde320ff43aa1cc59b45766155df25cbf737ca45
Aniket Singh <asaniket.singh422@gmail.com>
2026-08-22 13:01:49 +0530

    Ques8

--- StringLevel2/Ques8.java
+++ StringLevel2/Ques8.java
@@ -1,5 +1,17 @@
-package StringLevel2;
+
 
 public class Ques8 {
-    
+    public static void main(String[] args) {
+        String s="abc";
+        int count=0;
+
+        for(int i=0;i<s.length();i++){
+            for(int j=i;j<s.length();j++){
+                String sub=s.substring(i,j+1);
+
+                if(sub.charAt(0)==(sub.charAt(sub.length()-1)))count++;
+            }
+        }
+        System.out.println(count);
+    }
 }
