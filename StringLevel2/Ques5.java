f8ef5b8e87b6da704ee287f24f53b16ddc4ad53a
Aniket Singh <asaniket.singh422@gmail.com>
2026-08-22 12:43:43 +0530

    Ques5

--- StringLevel2/Ques5.java
+++ StringLevel2/Ques5.java
@@ -1,5 +1,14 @@
-package StringLevel2;
+
 
 public class Ques5 {
-    
+    public static void main(String[] args) {
+        String s=" Hello world ";
+        int count=0;
+        for(int i=0;i<s.length();i++){
+            if(s.charAt(i)==' '){
+                count++;
+            }
+        }
+        System.out.println(count);
+    }
 }
