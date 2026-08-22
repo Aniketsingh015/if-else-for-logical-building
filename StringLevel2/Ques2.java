a9d9ebb7c5bc1e088e929533fe9a52ab7e7ea238
Aniket Singh <asaniket.singh422@gmail.com>
2026-08-22 12:23:39 +0530

    ques2

--- StringLevel2/Ques2.java
+++ StringLevel2/Ques2.java
@@ -1,5 +1,28 @@
-package StringLevel2;
+
 
 public class Ques2 {
-    
+
+    public static void main(String[] args) {
+        String s="Aeloo World";
+        int digitCount=0;
+        int letterCount=0;
+        int specialCharacter=0;
+        
+        for(int i=0;i<s.length();i++){
+            char c=s.charAt(i);
+            if(Character.isLetter(c)){
+                letterCount++;
+            }else if(Character.isDigit(c)){
+                digitCount++;
+            }else{
+                specialCharacter++;
+            }
+        
+    }
+    System.out.println(digitCount);
+        System.out.println(letterCount);
+        System.out.println(specialCharacter);
 }
+}
+
+
