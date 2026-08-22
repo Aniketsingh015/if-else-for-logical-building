fb5141f4bd927c65074fdab9cc6b45455a792187
Aniket Singh <asaniket.singh422@gmail.com>
2026-08-22 12:18:55 +0530

    ques1

--- StringLevel2/Ques1.java
+++ StringLevel2/Ques1.java
@@ -1,5 +1,21 @@
-package StringLevel2;
+
 
 public class Ques1 {
-    
+    public static void main(String[] args) {
+        String s="Aeloo World";
+        int vowelCount=0;
+        int consonatCount=0;
+        
+        for(int i=0;i<s.length();i++){
+            char c=s.charAt(i);
+            c= Character.toLowerCase(c);
+            if(c=='a'||c=='e'||c=='i'||c=='o'||c=='u'){
+                vowelCount++;
+            }else if(Character.isLetter(c)){
+                consonatCount++;
+            }
+        }
+        System.out.println(vowelCount);
+        System.out.println(consonatCount);
+    }
 }
