c51e876c4d13d2bc36a75f83974fab244f09c311
Aniket Singh <asaniket.singh422@gmail.com>
2026-08-22 13:12:19 +0530

    push

--- StringLevel2/Ques10.java
+++ StringLevel2/Ques10.java
@@ -1,5 +1,16 @@
-package StringLevel2;
-
 public class Ques10 {
-    
+    public static void main(String[] args) {
+        String s = "cats and dogs chase birds";
+        int count = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            char v = Character.toLowerCase(s.charAt(i));
+
+            if ((i == s.length() - 1 || s.charAt(i + 1) == ' ') && v == 's') {
+                count++;
+            }
+        }
+
+        System.out.println(count);
+    }
 }
