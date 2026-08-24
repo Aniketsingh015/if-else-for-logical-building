d8ac73c08e4576428edd8f0b17f0d9f3cb1b5e2b
Aniket Singh <asaniket.singh422@gmail.com>
2026-08-24 13:09:13 +0530

    push

---
+++ StringLevel3/Ques3.java
@@ -0,0 +1,17 @@
+// Reverse the order of words in a sentence.
+
+public class Ques3 {
+    public static void main(String[] args) {
+        String s= "I am learning java";
+        StringBuilder sb=new StringBuilder();
+
+        String[] word=s.split(" ");
+        // this will soit the char into array as well in order
+
+        for(int i=word.length-1;i>=0;i--){
+            sb.append(word[i]);
+            sb.append(' ');
+        }
+        System.out.println(sb.toString());
+    }
+}
