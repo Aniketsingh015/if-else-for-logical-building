f89517ae9be8bf56fdc625cdcfe0245c031c183e
Aniket Singh <asaniket.singh422@gmail.com>
2026-08-24 13:29:43 +0530

    push

---
+++ StringLevel3/Ques8.java
@@ -0,0 +1,8 @@
+// Remove the first and last character and print the remaining string.
+
+public class Ques8 {
+    public static void main(String[] args) {
+        String s = "hello";
+        System.out.println(s.substring(1, s.length() - 1));
+    }
+}
